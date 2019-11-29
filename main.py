c7be14384311c72ef1bb52b7957e92499501aa73
Orel Saada <orelmishakim@gmail.com>
2019-11-29 16:08:32 +0200

    login and logout system

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from flask_login import UserMixin, LoginManager, current_user, login_required, login_user, logout_user
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, ValidationError
 
@@ -9,7 +10,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SECRET_KEY'] = '123'
 db = SQLAlchemy(app)
-
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+login_manager.init_app(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -33,10 +37,14 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(10), unique=True, index=True)
     password = db.Column(db.String(20), unique=True)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
     
 
 @app.route("/", methods=['GET', 'POST'])
@@ -48,8 +56,6 @@ def register():
         db.session.commit()
         flash('Registered!')
         return redirect(url_for('login'))
-    # if request.method == 'POST':
-    #     flash('Could not registerd.')
     return render_template('register.html', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -59,6 +65,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
             if form.password.data == user.password:
+                login_user(user)
                 return redirect(url_for('todo'))
     if request.method == 'POST':
         if not user:
@@ -67,6 +74,11 @@ def login():
             flash('Incorrect Password / Username.')
     return render_template('login.html', form=form)
 
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route("/database")
 def database():
@@ -79,9 +91,12 @@ def resetdb():
     return redirect(url_for('database'))
 
 @app.route("/todo")
+@login_required
 def todo():
     todos = Todo.query.all()
-    return render_template('todo.html', todos=todos)
+    user  = current_user
+    return render_template('todo.html', todos=todos, user=user)
+
 
 
 @app.route("/add", methods=['POST'])
