d97fb85dfc98b1985aade69ae27fcd831d688aaa
Orelsaada <orel.amedi@gmail.com>
2019-12-18 23:29:17 +0200

    added groups

--- main.py
+++ main.py
@@ -39,11 +39,41 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
+class CreateGroupForm(FlaskForm):
+    group_name = StringField('Group Name', validators=[DataRequired(), Length(min=2)])
+    group_password = PasswordField('Group Password', validators=[DataRequired()])
+    create = SubmitField('Create')
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(10), unique=True, index=True)
     password = db.Column(db.String(20))
     todos = db.relationship('Todo', backref='owner', lazy=True)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.order_by = User.username
+    
+    def __repr__(self):
+        return self.username
+
+groups = db.Table('groups',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('group_id', db.Integer, db.ForeignKey('group.id')))
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(10), unique=True, index=True)
+    password = db.Column(db.String(20))
+    users = db.relationship('User',
+                            secondary=groups,
+                            backref=db.backref('groups',
+                                                lazy='dynamic',
+                                                order_by=name))
+    def __repr__(self):
+        return self.name
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -85,7 +115,8 @@ def logout():
 def database():
     if current_user.username == 'admin':
         users = User.query.all()
-        return render_template('database.html', users=users)
+        groups = Group.query.all()
+        return render_template('database.html', users=users, groups=groups)
     abort(401)
 
 @app.route("/resetdb", methods=['POST', 'GET'])
@@ -99,7 +130,6 @@ def mytodo():
     user  = current_user
     todos = Todo.query.filter_by(user_id = user.id)
     return render_template('mytodo.html', todos=todos, user=user)
-
 
 
 @app.route("/add", methods=['POST'])
@@ -130,12 +160,30 @@ def share(todo_id):
     db.session.commit()
     return redirect(url_for('sharedtodo'))
 
+@app.route("/creategroup", methods=['POST', 'GET'])
+def create_group():
+    form = CreateGroupForm()
+    if form.validate_on_submit():
+        group = Group(name = form.group_name.data, password = form.group_password.data)
+        db.session.add(group)
+        db.session.commit()
+        add_owner_to_group()
+        db.session.commit()
+        flash('New Group Created!')
+        return redirect(url_for('database'))
+    return render_template('create_groups.html', form=form, title='Create New Group')
 
 
 def reset_database():
     users = User.query.delete()
     db.session.commit()
-    
+
+def add_owner_to_group():
+    user = current_user
+    groups = Group.query.all()
+    last_group = groups[-1]
+    last_group.users.append(user)    
 
 if __name__ == '__main__':
     app.run(debug=True)
+
